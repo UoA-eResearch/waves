70b069a30f38daeb3f6a9ca3273bdcd2852c3797
Nick <nick.young@auckland.ac.nz>
2019-01-24 15:09:21 +1300

    fix latlongs, add script to output latlongs

---
+++ print_latlongs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import scipy.io
+import json
+
+nimat = scipy.io.loadmat("data/NI-930101_931231-PDIR.mat")
+simat = scipy.io.loadmat("data/SI-930101_931231-PDIR.mat")
+
+latlongs = {
+    "ni": {
+        "lat": nimat["Yp"].tolist(),
+        "lng": nimat["Xp"].tolist()
+    },
+    "si": {
+        "lat": simat["Yp"].tolist(),
+        "lng": simat["Xp"].tolist()
+    }
+}
+
+print(json.dumps(latlongs))
