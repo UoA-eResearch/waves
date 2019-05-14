8c5079cf2bc0b313224cec19f830166a720780da
Nick <nick.young@auckland.ac.nz>
2019-05-14 15:22:52 +1200

    temporarily disable Depth filtering

--- custom.js
+++ custom.js
@@ -520,8 +520,8 @@ function fetchRanges() {
                     }
                     var lat = data.latlongs[island].lat[i][j];
                     var lng = data.latlongs[island].lng[i][j];
-                    var depth = data.depth[island][i][j];
-                    if (depth < 10) continue;
+                    //var depth = data.depth[island][i][j];
+                    //if (depth < 10) continue;
                     var desc = island.toUpperCase() + ":(" + lat.toFixed(dp) + "°," + lng.toFixed(dp) + "°)/(" + i + "," + j + ")";
                     var progress = '<div class="progress">';
                     progress += '<div id="chartprogress" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0%" aria-valuemin="0%" aria-valuemax="100%" style="width: 0%">';
