b0214afa4a0842d3a87d6707b5321053bf10fc80
Nick <nick.young@auckland.ac.nz>
2019-05-16 10:53:21 +1200

    ensure depth for points that are not offshore is 0

--- print_depth.py
+++ print_depth.py
@@ -2,6 +2,18 @@
 import scipy.io
 import json
 import numpy as np
+import csv
+
+lloffshore = {}
+
+with open('latlongs_offshore.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for r in reader:
+      lat = round(float(r["lat"]), 6)
+      lng = round(float(r["long"]), 6)
+      if lat not in lloffshore:
+        lloffshore[lat] = {}
+      lloffshore[lat][lng] = r["offshore"]
 
 def process_mat(mat):
     mat = np.where(np.isnan(mat), None, mat)
@@ -10,6 +22,23 @@ def process_mat(mat):
 
 nimat = scipy.io.loadmat("data/NI-100701_101231-DEPTH.mat")
 simat = scipy.io.loadmat("data/SI-100701_101231-DEPTH.mat")
+
+nishape = nimat["Yp"].shape
+sishape = simat["Yp"].shape
+
+for i in range(nishape[0]):
+    for j in range(nishape[1]):
+        lat = round(float(nimat["Yp"][i][j]), 6)
+        lng = round(float(nimat["Xp"][i][j]), 6)
+        if lloffshore[lat][lng] == "0":
+          nimat["Depth_20100628_120000"][i][j] = None
+
+for i in range(sishape[0]):
+    for j in range(sishape[1]):
+        lat = round(float(simat["Yp"][i][j]), 6)
+        lng = round(float(simat["Xp"][i][j]), 6)
+        if lloffshore[lat][lng] == "0":
+          simat["Depth_20100628_120000"][i][j] = None
 
 depths = {
   "ni": process_mat(nimat["Depth_20100628_120000"]),
