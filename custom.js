cb5dc3e114f56f3b6e755010b9ad1bbfcaa44440
Nick Young <nick.young@auckland.ac.nz>
2022-01-11 14:51:08 +1300

    Add message about data update

--- custom.js
+++ custom.js
@@ -922,6 +922,16 @@ var variable = "HSIGN-Hsig";
 if ($("#model").length) {
     model = $("#model").val();
 }
+if (model != "NZ-HIST-000") {
+    alert(`Kia ora!
+
+Thanks for your interest in our site and our data - it is greatly appreciated!
+
+We are currently updating our 21st century projections database. This is a very long process, which requires most of our server's capacity. This means that data download will be very slow, so please be patient while we are working on that.
+    
+Yours sincerely
+Coast and Ocean Collective`)
+}
 if (location.hash.length > 1) {
     var bits = decodeURIComponent(location.hash.slice(1)).split("@");
     var model_var = bits[0];
